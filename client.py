c141b49192df750589bbaacafdeeb42c2a83f6cc
Spencer? <spencer.martel4@gmail.com>
2022-01-23 11:07:33 -0500

    initial client code-working
    Connecting to test server.

--- client.py
+++ client.py
@@ -1,1 +1,15 @@
-# Hey Spencer, client code here buuuuuddy
+import discord
+
+client = discord.Client()
+token = 'OTM0ODM2NzAzMzkzMzUzNzcx.Ye14hg.LaW_DkWQWKVSXJqWWSB9bhcNrVU'
+#this is the password for the bot to enter the discord server, you have to give it access to the server on the discord developer portal
+
+
+@client.event
+async def on_ready():
+    print('---------------------------------------------')
+    print('We have logged in as {0.user}'.format(client))
+    print('---------------------------------------------')
+    return
+
+client.run(token)
